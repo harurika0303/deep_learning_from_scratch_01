3f73b33331bdbe65892ac3499ce9ea4e474751c5
harurika0303 <harurika3333@gmail.com>
2025-04-26 12:04:49 +0000

    AND,OR,NANDを実装

---
+++ src/perceptron.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    return 1 if np.sum(x*w) + b else 0
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    return 1 if np.sum(x*w) + b else 0
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    return 1 if np.sum(x*w) + b else 0
+
+
